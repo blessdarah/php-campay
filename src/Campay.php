cdbd9dc2fa8df942361cc4cdde7d725647d3faf8
user <blessdarahuba@gmail.com>
2024-07-17 21:12:53 +0100

    chore: ensure application is stable

--- src/Campay.php
+++ src/Campay.php
@@ -9,24 +9,25 @@ use GuzzleHttp\Exception\GuzzleException;
 class Campay
 {
     private string $token;
+
     private array $headers;
+
     private Client $client;
 
     /**
      * constructor
      */
-    public function __construct($base_url = "https://demo.campay.net/api/")
+    public function __construct($base_url = 'https://demo.campay.net/api/')
     {
         $this->client = new Client([
             'base_uri' => $base_url,
-            'timeout' => 30 // 30 sec timeout
+            'timeout' => 30, // 30 sec timeout
         ]);
 
         $this->token = $this->request_token();
     }
 
     /**
-     * @param array $headers
      * @returns void
      */
     public function setHeaders(array $headers): void
@@ -34,24 +35,17 @@ class Campay
         $this->headers = $headers;
     }
 
-    /**
-     * @return array
-     */
     public function getHeaders(): array
     {
         return $this->headers;
     }
 
-    /**
-     * @return string
-     */
     public function getToken(): string
     {
         return $this->token;
     }
 
     /**
-     * @param string $token
      * @returns void
      */
     public function setToken(string $token): void
@@ -61,113 +55,111 @@ class Campay
 
     /**
      * @return mixed
+     *
      * @throws GuzzleException
      */
     private function request_token()
     {
         $config = [
-            "username" => $_ENV['CAMPAY_USERNAME'],
-            "password" => $_ENV['CAMPAY_PASSWORD']
+            'username' => $_ENV['CAMPAY_USERNAME'],
+            'password' => $_ENV['CAMPAY_PASSWORD'],
         ];
 
         $options = [
-            "form_params" => $config
+            'form_params' => $config,
         ];
 
         $response = $this->client->post('token/', $options);
         $data = json_decode($response->getBody());
         $this->setHeaders([
-            "Authorization" => "Token " . $data->token
+            'Authorization' => 'Token '.$data->token,
         ]);
+
         return $data->token;
     }
 
-
     /**
-     * @param array $data
      * @throws GuzzleException
      */
     public function collect(array $data)
     {
-        $uri = "collect/";
-        $response = $this->client->post($uri, array("form_params" => $data, "headers" => $this->getHeaders()));
+        $uri = 'collect/';
+        $response = $this->client->post($uri, ['form_params' => $data, 'headers' => $this->getHeaders()]);
+
         return $response->getBody()->getContents();
     }
 
     /**
-     * @param string $reference
      * @throws GuzzleException
      */
     public function getTransactionStatus(string $reference)
     {
-        $uri = "transaction/" . $reference . "/";
+        $uri = 'transaction/'.$reference.'/';
         $response = $this->client->get($uri, [
-            "headers" => $this->getHeaders()
+            'headers' => $this->getHeaders(),
         ]);
+
         return $response->getBody()->getContents();
     }
 
     /**
-     * @param array $data
      * @throws GuzzleException
      */
     public function withdraw(array $data)
     {
-        $uri = "withdraw/";
-        $response = $this->client->post($uri, array("form_params" => $data, "headers" => $this->getHeaders()));
+        $uri = 'withdraw/';
+        $response = $this->client->post($uri, ['form_params' => $data, 'headers' => $this->getHeaders()]);
+
         return $response->getBody()->getContents();
     }
-
 
     /**
      * @throws GuzzleException
      */
     public function getAppBalance()
     {
-        $uri = "balance/";
-        $response = $this->client->get($uri, array("headers" => $this->getHeaders()));
+        $uri = 'balance/';
+        $response = $this->client->get($uri, ['headers' => $this->getHeaders()]);
+
         return $response->getBody()->getContents();
     }
 
-
     /**
-     * @param $start
-     * @param $end
      * @throws GuzzleException
      */
     public function transactionHistory($start = null, $end = null)
     {
-        $uri = "history/";
+        $uri = 'history/';
         $date = new Carbon();
-        if(!isset($start)) {
+        if (! isset($start)) {
             $start = $date->subWeek()->format('Y-m-d');
         }
 
-        if(!isset($end)) {
+        if (! isset($end)) {
             $end = Carbon::now()->format('Y-m-d');
         }
 
         $params = [
-            "start_date" => date_format(new \DateTime($start), "Y-m-d"),
-            "end_date" => date_format(new \DateTime($end), "Y-m-d"),
+            'start_date' => date_format(new \DateTime($start), 'Y-m-d'),
+            'end_date' => date_format(new \DateTime($end), 'Y-m-d'),
         ];
 
-        $response = $this->client->post($uri, array("form_params" => $params, "headers" => $this->getHeaders()));
+        $response = $this->client->post($uri, ['form_params' => $params, 'headers' => $this->getHeaders()]);
+
         return $response->getBody()->getContents();
     }
 
-
     /**
-     * @param array $params
      * @throws GuzzleException
      */
     public function generatePaymentUrl(array $params)
     {
-        $uri = "get_payment_link/";
-        $response = $this->client->post($uri, array(
-            "form_params" => $params,
-            "headers" => $this->getHeaders()
-        ));
+        $uri = 'get_payment_link/';
+        $response = $this->client->post($uri, [
+            'form_params' => $params,
+            'headers' => $this->getHeaders(),
+        ]);
+
         return $response->getBody();
     }
 }
